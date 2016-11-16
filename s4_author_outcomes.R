0614b636c4d754c2bb445e8328254e7a078c1a3c
C. Sean Burns <sean.csb@gmail.com>
2016-11-16 14:32:52 -0500

    updated analysis

--- s4_author_outcomes.R
+++ s4_author_outcomes.R
@@ -1,24 +1,32 @@
 # Authors by regions and comparing to paper rejections
+# Focus on data set filtered by sent for review
 
 source("libraries.R")
 
-dec0 <- dec
+dec_sent$paper_rejected       <- relevel(dec_sent$paper_rejected, ref = "Yes")
+dec_sent$first_auth_geog      <- relevel(dec_sent$first_auth_geog, ref = "Europe")
 
-dec0$paper_rejected       <- relevel(dec0$paper_rejected, ref = "Yes")
-dec0$first_auth_geog      <- relevel(dec0$first_auth_geog, ref = "Europe")
+contrasts(dec_sent$paper_rejected)
+contrasts(dec_sent$first_auth_geog)
 
-contrasts(dec0$paper_rejected)
-contrasts(dec0$first_auth_geog)
-
-fit.0 <- glm(paper_rejected ~ first_auth_geog, data = dec0, family = "binomial")
+fit.0 <- glm(paper_rejected ~ first_auth_geog, data = dec_sent, family = "binomial")
 summary(fit.0)
 round(exp(cbind(OR = coef(fit.0), confint(fit.0))), 3)
 
-ggplot(dec0, aes(x = first_auth_geog, fill = paper_rejected)) +
-        geom_bar() + theme_bw() + scale_fill_grey(name = "Paper Rejected") +
-        xlab("Geographical Region of First Author") +
-        ylab("Count") +
-        theme(legend.position = c(.9,.8))
+reorder_size <- function(x) {
+        factor(x, levels = names(sort(table(x), decreasing = TRUE)))
+}
+
+ggplot(dec_sent, aes(x = reorder_size(first_auth_geog), fill = paper_rejected)) +
+        geom_bar() + theme_bw() + 
+        scale_fill_grey(name = "Revision Invited / Declined") +
+        labs(x = "Geographical Region of First Author",
+             y = "Count") +
+        theme(axis.text.y = element_text(size = 12,
+                                         colour = "black")) +
+        theme(axis.text.x = element_text(size = 12,
+                                         colour = "black")) +
+        theme(legend.position = c(.8,.8))
 
 # Test the overall effect of the levels
 wald.test(b = coef(fit.0), Sigma = vcov(fit.0), Terms = 2:7)
@@ -34,23 +42,21 @@ chisq.prob  <- 1 - pchisq(fit.chi, chi.df)
 # display the results
 fit.chi ; chi.df ; chisq.prob
 
-dec0$prob   <- predict(fit.0, type = c("response"))
+dec_sent$prob   <- predict(fit.0, type = c("response"))
 
-g <- roc(paper_rejected ~ prob, data = dec0) ; g
+g <- roc(paper_rejected ~ prob, data = dec_sent) ; g
 plot(g)
 
-rm(dec0, fit.0, fit.chi, chi.df, chisq.prob, g)
+rm(dec_sent, fit.0, fit.chi, chi.df, chisq.prob, g)
 
 ### test if multinational authorships are penalized ###
 ### identifies all manuscripts where authors are from the same nation and
 ### authors are from different nations
 ### need to remove single authorships ###
 
-dec0 <- dec # start over
-
-# create new database with manuscript IDs, paper rejection status, and author
+# create new dataframe with manuscript IDs, paper rejection status, and author
 # country.
-dec_handling_editors <- select(dec0, ms_id, paper_rejected)
+dec_handling_editors <- select(dec_sent, ms_id, paper_rejected)
 dec_author_country   <- select(author_decisions, ms_id, author_country)
 dec_mixed_countries  <- inner_join(dec_handling_editors,
                                    dec_author_country,
@@ -58,7 +64,7 @@ dec_mixed_countries  <- inner_join(dec_handling_editors,
 dec_mixed_countries  <- distinct(dec_mixed_countries)
 dec_mixed_countries$mixed  <- duplicated(dec_mixed_countries$ms_id)
 
-rm(dec0, dec_handling_editors, dec_author_country)
+rm(dec_handling_editors, dec_author_country)
 
 mixed.true  <- dec_mixed_countries %>% filter(mixed == TRUE)
 mixed.false <- dec_mixed_countries %>% filter(mixed == FALSE)
@@ -87,4 +93,4 @@ mixed.combined <- mixed.combined[!(mixed.combined$ms_id %in% singles),]
 chisq.test(table(mixed.combined$mixed))
 chisq.test(table(mixed.combined$paper_rejected, mixed.combined$mixed))
 
-rm(mixed.true, mixed.false, mixed.false.logic, singles, singles.df)
+rm(mixed.true, mixed.false, mixed.false.logic, singles, singles.df, mixed.combined)
