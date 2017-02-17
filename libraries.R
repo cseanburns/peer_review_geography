4d025933d679b15afa01562cc4041fd11faa3054
C. Sean Burns <sean.csb@gmail.com>
2017-02-17 13:26:35 -0500

    added car & removed mice packages

--- libraries.R
+++ libraries.R
@@ -1,10 +1,10 @@
 # Libraries needed
 
 library("aod")
+library("car")
 #library("plyr")
 library("dplyr")
 library("reshape2")
 library("vcd")
 #library("ggplot2")
 #library("MASS")
-#library("mice")
