aa95ba87f6f260ef2ba991f2fc56030bcf940917
C. Sean Burns <sean.csb@gmail.com>
2017-03-20 10:59:36 -0400

    added analysis on all papers

--- s6_mixed.R
+++ s6_mixed.R
@@ -5,6 +5,70 @@
 ### mixed.combined$mixed = FALSE, means that authors are located in same country
 ### mixed.combined$mixed = TRUE, means that authors are located in different countries
 
+## for all papers
+
+dec_handling_editors <- dec %>% select(ms_id, sent_for_review)
+dec_author_country   <- author_decisions %>% select(ms_id, geographic_region)
+dec_mixed_countries  <- inner_join(dec_handling_editors,
+                                   dec_author_country,
+                                   by = "ms_id")
+dec_mixed_countries  <- distinct(dec_mixed_countries)
+dec_mixed_countries$mixed  <- duplicated(dec_mixed_countries$ms_id)
+
+rm(dec_handling_editors, dec_author_country)
+
+mixed.true  <- dec_mixed_countries %>% filter(mixed == TRUE)
+mixed.false <- dec_mixed_countries %>% filter(mixed == FALSE)
+
+rm(dec_mixed_countries)
+
+mixed.true$geographic_region <- NULL
+mixed.false$geographic_region <- NULL
+
+mixed.false.logic <- mixed.false[!(mixed.false$ms_id %in% mixed.true$ms_id),]
+mixed.combined    <- rbind(mixed.true, mixed.false.logic)
+mixed.combined    <- unique(mixed.combined)
+
+# identify single authorships
+singles.df <- select(author_decisions, ms_id)
+singles.df <- data.frame(table(singles.df))
+singles.df <- singles.df %>% filter(Freq == 1)
+singles.df$singles.df <- as.integer(as.character(singles.df$singles.df))
+singles <- singles.df$singles.df
+
+# remove singler authorships from data for analysis
+
+mixed.combined <- mixed.combined[!(mixed.combined$ms_id %in% singles),]
+
+rm(mixed.false, mixed.false.logic, mixed.true, singles.df, singles)
+
+# run chi square test on mixed authorship and then on rejection status
+table(mixed.combined$mixed)
+table(mixed.combined$sent_for_review)
+table(mixed.combined$sent_for_review, mixed.combined$mixed)
+plot(mixed.combined$sent_for_review, mixed.combined$mixed)
+
+p <- table(mixed.combined$mixed) / sum(table(mixed.combined$mixed))
+q <- table(mixed.combined$sent_for_review)
+chisq.test(q, p = p)
+boxplot(q, p)
+
+# regression
+table(mixed.combined$mixed, mixed.combined$sent_for_review)
+mixed.combined$mixed <- as.factor(mixed.combined$mixed)
+mixed.combined$sent_for_review <- relevel(mixed.combined$sent_for_review, ref = "No")
+mixed.combined$mixed          <- relevel(mixed.combined$mixed, ref = "FALSE")
+contrasts(mixed.combined$sent_for_review)
+contrasts(mixed.combined$mixed)
+
+fit.0 <- glm(sent_for_review ~ mixed,
+             data = mixed.combined, family = "binomial")
+summary(fit.0)
+round(exp(cbind(OR = coef(fit.0), confint(fit.0))), 3)
+Anova(fit.0)
+
+rm(fit.0, mixed.combined, p, q)
+
 # create new dataframe with manuscript IDs, paper rejection status, and author
 # country.
 
@@ -48,8 +112,19 @@ mixed.combined <- mixed.combined[!(mixed.combined$ms_id %in% singles),]
 rm(mixed.false, mixed.false.logic, mixed.true, singles.df, singles)
 
 # run chi square test on mixed authorship and then on rejection status
-chisq.test(table(mixed.combined$mixed))
-chisq.test(table(mixed.combined$paper_rejected, mixed.combined$mixed))
+# chisq.test(table(mixed.combined$mixed))
+# chisq.test(table(mixed.combined$paper_rejected, mixed.combined$mixed))
+
+# run chi square test on mixed authorship and then on rejection status
+table(mixed.combined$mixed)
+table(mixed.combined$paper_rejected)
+table(mixed.combined$paper_rejected, mixed.combined$mixed)
+plot(mixed.combined$paper_rejected, mixed.combined$mixed)
+
+p <- table(mixed.combined$mixed) / sum(table(mixed.combined$mixed))
+q <- table(mixed.combined$paper_rejected)
+chisq.test(q, p = p)
+boxplot(q, p)
 
 # regression
 table(mixed.combined$mixed, mixed.combined$paper_rejected)
@@ -63,16 +138,6 @@ fit.0 <- glm(paper_rejected ~ mixed,
              data = mixed.combined, family = "binomial")
 summary(fit.0)
 round(exp(cbind(OR = coef(fit.0), confint(fit.0))), 3)
+Anova(fit.0)
 
-# The reduction in the deviance; results in the chi square statistic
-fit.chi <- fit.0$null.deviance - fit.0$deviance
-# The degrees of freedom for the chi square statistic
-chi.df  <- fit.0$df.null - fit.0$df.residual
-# The probability associated with the chi-square statistic; If (e.g.) less than
-# 0.05, we can reject the null hypothesis that the model is not better than
-# chance at predicting the outcome
-chisq.prob <- 1 - pchisq(fit.chi, chi.df)
-# Display the results
-fit.chi; chi.df; chisq.prob
-
-rm(chi.df, chisq.prob, fit.0, fit.chi, singles)
+rm(fit.0, mixed.combined)
