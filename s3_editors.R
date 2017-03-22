22b58d3a32fd1ed3c5686fe0bec3eb526c737102
C. Sean Burns <sean.csb@gmail.com>
2017-03-22 12:39:11 -0400

    minor code revisions

--- s3_editors.R
+++ s3_editors.R
@@ -1,9 +1,7 @@
 source("libraries.R")
 
 # Editors by geographic regions, comparing to sent for review
-
-dec0 <- dec
-dec0 <- select(dec0, sent_for_review, handling_editor_geog, handling_editor)
+dec0 <- select(dec, sent_for_review, handling_editor_geog, handling_editor)
 dec0 <- dec0[complete.cases(dec0),]
 dec0 <- filter(dec0, handling_editor_geog != "Latin America")
 
@@ -19,6 +17,7 @@ fit.0 <- glm(sent_for_review ~ handling_editor_geog, data = dec0, family = "bino
 summary(fit.0)
 round(exp(cbind(OR = coef(fit.0), confint(fit.0))), 3)
 table(dec0$sent_for_review, dec0$handling_editor_geog)
+Anova(fit.0)
 
 # Add handling_editor ID as a random effect
 # Code derived from: http://stats.idre.ucla.edu/r/dae/mixed-effects-logistic-regression/
@@ -45,17 +44,6 @@ wald.test(b = coef(fit.0), Sigma = vcov(fit.0), Terms = 5)
 wald.test(b = coef(fit.0), Sigma = vcov(fit.0), Terms = 6)
 wald.test(b = coef(fit.0), Sigma = vcov(fit.0), Terms = 2:6)
 
-# The reduction in the deviance; results in the chi square statistic
-fit.chi     <- fit.0$null.deviance - fit.0$deviance
-# The degrees of freedom for the chi square statistic
-chi.df      <- fit.0$df.null - fit.0$df.residual
-# The probability associated with the chi-square statisitc
-# If (e.g.) less than 0.05, then we can reject the null hypothesis that the model
-# is not better than chance at predicting the outcome
-chisq.prob  <- 1 - pchisq(fit.chi, chi.df) 
-# Display the results
-fit.chi ; chi.df ; chisq.prob
-
 # ROC Curve
 roc_curve <- function(model, dataset) {
         prob <- predict(model, type = c("response"))
@@ -71,9 +59,7 @@ rm(fit.0, fit.1, dec0, fit.chi, chi.df, chisq.prob, roc_curve, tab, se)
 
 # Editors by geographic regions, comparing to paper rejections 
 # Focus on sent for review data and not all data
-
-dec0 <- dec
-dec_sent <- filter(dec0, sent_for_review == "Yes")
+dec_sent <- filter(dec, sent_for_review == "Yes")
 
 # Get percentage of handling editors by region
 sort(table(dec_sent$handling_editor_geog), decreasing = TRUE)
@@ -160,24 +146,6 @@ roc_curve <- function(model, dataset) {
         pg <- plot(g)
         return(list(plot(pg)))
 }
-
-# dec_tmp <- select(dec_sent, handling_editor_geog, paper_rejected)
-# table(dec_tmp)
-# 
-# reorder_size <- function(x) {
-#         factor(x, levels = names(sort(table(x), decreasing = TRUE)))
-# }
-# 
-# ggplot(dec_tmp, aes(x = reorder_size(handling_editor_geog), fill = paper_rejected)) +
-#         geom_bar(stat = "count") + theme_bw() +
-#         scale_fill_grey(name = "Revision Invited / Declined") +
-#         labs(x = "Geographical Region of Handling Editor",
-#         y = "Count") +
-#         theme(axis.text.y = element_text(size = 12,
-#                                     colour = "black")) +
-#         theme(axis.text.x = element_text(size = 12,
-#                                     colour = "black")) +
-#         theme(legend.position = c(.8,.8))
 
 rm(dec_sent, dec0, dec_sent, fit.0, fit.chi, chi.df, chisq.prob)
 rm(tab, fit.1, p, q, se)
