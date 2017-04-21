2c096781f56ca58cefeff38c7661c09178edd7f0
C. Sean Burns <sean.csb@gmail.com>
2017-04-21 15:54:00 -0400

    updated and formatted

--- libraries.R
+++ libraries.R
@@ -2,12 +2,12 @@
 
 library("aod")
 library("car")
-#library("plyr")
 library("dplyr")
 library("lme4")
-library("lsmeans")
 library("pROC")
 library("reshape2")
 library("vcd")
+
 #library("ggplot2")
 #library("MASS")
+#library("plyr")
